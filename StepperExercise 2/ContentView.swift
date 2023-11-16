6ec3324f6ce6de0a7c37a92c3ff183cbb7c055f1
Edna Sung <esung@lcs.on.ca>
2023-11-16 16:29:26 -0500

    made the page of plus.

--- StepperExercise 2/ContentView.swift
+++ StepperExercise 2/ContentView.swift
@@ -8,14 +8,62 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var base1: Int = 1
+    
+    @State var base2: Int = 1
+    
+    var sum: Int {
+        return base1 + base2
+    }
     var body: some View {
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+ 
+            HStack(alignment: .top) {
+                
+                Spacer()
+                
+                Text("\(base1)")
+                    .font(.system(size: 60))
+            }
+            .padding()
+            
+            Stepper(value: $base1, label: {
+                Text("select first number ")
+            })
+            
+            .padding()
+            
+            
+            HStack {
+                Text("+")
+                    .font(.system(size: 60))
+                
+                Spacer()
+                
+                Text("\(base2)")
+                    .font(.system(size: 60))
+                
+            }
+            .padding()
+            
+            Stepper(value: $base2, label: {
+                Text("select second number ")
+            })
+            
+            .padding()
+            
+            Divider()
+            
+            HStack {
+                
+                Spacer()
+                
+                Text("\(sum)")
+                    .font(.system(size: 60))
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
