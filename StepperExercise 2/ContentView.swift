2750b21615b52fb64e6df97b2a107e8feb7dcd1d
Edna Sung <esung@lcs.on.ca>
2023-11-16 09:55:08 -0500

    Initial Commit

---
+++ StepperExercise 2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StepperExercise 2
+//
+//  Created by Edna Sung on 2023-11-16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StepperExercise 2/StepperExercise_2App.swift
@@ -0,0 +1,17 @@
+//
+//  StepperExercise_2App.swift
+//  StepperExercise 2
+//
+//  Created by Edna Sung on 2023-11-16.
+//
+
+import SwiftUI
+
+@main
+struct StepperExercise_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
